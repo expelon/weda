b57a5eadcec8ba4dc0ace90894b60b2ac099d1df
shabinvs <expelonofficial@gmail.com>
2026-02-13 01:47:46 +0530

    change 1 icon

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -126,7 +126,7 @@ export function HomePage() {
       instagram: 'https://www.instagram.com/rathishrinivasan?igsh=cTFrZG9xMXMxcDEy',
       icon: (
         <svg className="h-6 w-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
         </svg>
       ),
     },
